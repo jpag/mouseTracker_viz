57a194316ae240be732281c13b0d30efa8ab70f6
Jp Gary <jp@stinkdigital.com>
2012-04-12 20:06:51 -0400

    running demo with circles

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,76 @@
 var socket
 
+
+var listOfFollowers = [];
+var heightOfFollower = 48;
+var widthOfFollower = 48;
+
+
+
 var c = new Object();
 
-c.port = '1338';
-c.address = '24.103.78.238';
+c.port = '1337';
+c.address = '192.168.1.11';
 c.id = 0;
 
 c.init = function(){
 	
+	$.getScript("http://"+c.address+":"+c.port+"/socket.io/socket.io.js" , c.socketScriptComplete );
+	
+}
+c.socketScriptComplete = function(){
+
 	//create socket	
 	socket = io.connect('http://'+c.address+':'+c.port);
-	socket.on('news', function (data) {
-	  trace(data);
-	  socket.emit('my other event', { my: 'data' });
+	socket.on('appEvent', function (data) {
+	  //trace( data );
+	  
+	  var followerFound = false;
+	  for( var i = 0; i < listOfFollowers.length; i++){
+	  		if( data.id == listOfFollowers[i].id ){
+	  			followerFound = true;
+	  			break;
+	  		} 
+	  }
+	  	
+	  if( followerFound == false ){
+	  //new follower create one!
+	  		c.setupFollower( data );
+	  }else{
+	  	//follower already exists
+	  		c.updateFollower(data);
+	  }
+	  //socket.emit('htmlCallback', { my: 'data' });
 	});
 		
 }
+
+c.updateFollower = function(data){
+
+	var coords = data.coordinates.split("," );
+	trace('id ' + data.id + ' ' + coords[0] +' - ' + coords[1] );
+	//var divid = "f"+data.id.toString();
+	//trace( divid );
+	//trace( $(divid) );
+	
+	$( "#f"+data.id ).css({ left:coords[0]+"px", top:coords[1] });
+	
+}
+
+c.setupFollower = function(data){
+	var colorVal = data.color;
+	var id_val = data.id;
+	
+	trace(' CREATED ' + id_val + ' ----- colorVal: ' + colorVal );
+	$("#followerContainer").append("<div id='f"+id_val+"' class='follow' style='background-color:"+colorVal+"' ></div>" );
+	c.updateFollower(data);
+	trace(data );
+	trace(' /n-----');
+	listOfFollowers.push({id:id_val , color:colorVal});
+};
+
+
+
 
 
 $(window).resize(function(){ });
--- www/serverside/server.js
+++ www/serverside/server.js
@@ -7,11 +7,13 @@ var fs = require('fs');
 var sockets_list = [];
 
 
+var colors = ["#bbd100" , "#45d697" , "#45c7d6" , "#3d81ff" , "#8f3dff" , "#ff3def" , "#f80767" , "#ff3600" , "#c6ff00" , "#00ffde" ];
+
 
 //-----HTML clients
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+  socket.emit('data', { event: 'connect' });
+  socket.on('htmlCallback', function (data) {
     console.log(data);
   });
 });
@@ -19,14 +21,24 @@ io.sockets.on('connection', function (socket) {
 
 //-----APP clients
 var server = net.createServer(function (socket) {
-  sockets_list.push(socket);
-  socket.write('Echo server\r\n');
-  socket.setEncoding("ascii");
-  socket.on('data', function(data) {
-    console.log(data);
-    //sending data via socket io, to port 1337
-    io.sockets.emit('news', { hello: data });
-  });
+	var id = sockets_list.length+1;
+  	
+	socket.id = id;
+	var cid = Math.round( Math.random()*colors.length );
+	if( cid > (colors.length-1) ){ cid = 0; };
+	socket.color = colors[ cid ];
+	
+	console.log( id + ' color picked ' + socket.color );
+	
+	sockets_list.push(socket);	  
+	socket.write('Echo server\r\n');
+	socket.setEncoding("ascii");
+	
+	socket.on('data', function(data) {
+    	//console.log(data);
+	    //sending data via socket io, to port 1337
+    	io.sockets.emit('appEvent', { coordinates:data , id: id , color:socket.color });
+	});
 
 });
 
