995d819a6c236663ef5d24658907ec585dff2df5
Jp Gary <jp@stinkdigital.com>
2012-04-27 16:39:09 -0400

    tweaks and adjustments to follow icon/alpha

--- www/js/app.js
+++ www/js/app.js
@@ -2,44 +2,114 @@ var socket
 
 
 var listOfFollowers = [];
-var heightOfFollower = 48;
-var widthOfFollower = 48;
+var heightOfFollower = 20;
+var widthOfFollower = 20;
 
-var canvasH = 1080;
 var canvasW = 1920;
+var canvasH = 1080;
 
+var increment = 0;
 
 var canvas = new Object();
 	canvas.w = 1920;
 	canvas.h = 1080;
+	canvas.r = 1;
 	canvas.ctx;
 	canvas.init = function(){
-		$('#visualizeContainer').append('<canvas id="canvas-draw" width="'+canvasW+'" height="'+canvasH+'"></canvas>');
+		canvas.w = canvasW;
+		canvas.h = canvasH;
+		
+		$('#visualizeContainer').append('<canvas id="canvas-draw" width="'+canvas.w+'" height="'+canvas.h+'"></canvas>');
 		//get a reference to the canvas
 		canvas.ctx = $('#canvas-draw')[0].getContext("2d");
+		
+		canvas.fadePreviousData();
+	}
+
+	canvas.lighten = function(col,amt) {
+//	    var num = parseInt(col,16);
+//	    var r = (num >> 16) + amt;
+//	    var b = ((num >> 8) & 0x00FF) + amt;
+//	    var g = (num & 0x0000FF) + amt;
+//	    var newColor = g | (b << 8) | (r << 16);
+//	    
+//	    return newColor.toString(16);
+		
+		col[0] += parseInt(amt);
+		col[1] += parseInt(amt);
+		col[2] += parseInt(amt);
+		
+		return col;
 	}
 	
 	canvas.drawPoint = function(data){
 		
-		//canvas.fadePreviousData();
+		//canvas.ctx.globalAlpha = data.alpha;
+		//trace( canvas.ctx.globalAlpha );
 		
-		canvas.ctx.fillStyle = data.color;
+		var color = toRGB( data.color.toString() );
+			//color = [ Math.round(Math.random()*250) , Math.round(Math.random()*250) , Math.round(Math.random()*250) ];
+		//var colorval = 'rgba('+canvas.lighten(color[0],data.alpha)+','+canvas.lighten(color[1],data.alpha)+','+canvas.lighten(color[2],data.alpha)+',1)';
+		
+		//var colorval = '#'+canvas.lighten( data.color , data.alpha );
+		var colorLight = canvas.lighten(color , data.alpha);
+		
+		var colorval = 'rgb('+colorLight[0]+','+colorLight[1]+','+colorLight[2]+');';
+		
+			//colorval = 'rgba(187,209,0,.001)';
+			//colorval = '#'+data.color.toString();
+		
+		
+		if( increment > 2000 && increment < 2100 ){
+			//trace( data.color.toString() );
+			trace( data.alpha  );
+			//trace( color );
+			trace( colorval );
+			trace( ' ---- ' );
+			
+		}
+		
+		increment++;
+
+		
+		canvas.ctx.fillStyle = colorval;//"#"+data.color;
 		canvas.ctx.beginPath();
-		canvas.ctx.arc(data.x, data.y, 5, 0, Math.PI*2, true); 
+		canvas.ctx.arc(data.x, data.y, data.radius, 0, Math.PI*2, true); 
 		canvas.ctx.closePath();
 		canvas.ctx.fill();
+		
+		//canvas.ctx.globalAlpha = 1;
 	}
 	
 	canvas.fadePreviousData = function(){
 		//http://beej.us/blog/data/html5s-canvas-2-pixel/
+		//http://jsfiddle.net/Gtvv6/
 		
-		 // get all canvas pixel data
-		    canvas.ctx.globalAlpha = canvas.ctx.globalAlpha * .9;
-		    var imageData = canvas.ctx.getImageData(0, 0, canvas.w, canvas.h);
-			canvas.ctx.globalAlpha = 1;
-			
+		trace(' fade previous ... ' );
+		
+		// get all canvas pixel data
+		//var imageData = canvas.ctx.getImageData(0, 0, canvas.w, canvas.h);
+		
+		var tempCanvas = document.createElement('canvas');
+		//no need to delete this it is in the reference
+		  tempCanvas.height = canvas.h;
+		  tempCanvas.width = canvas.w;
+		
+		var tempCanvasCTX = tempCanvas.getContext('2d');
+		tempCanvasCTX.drawImage($('#canvas-draw')[0] ,0,0);
+		
+		//clear visible canvas
+		canvas.ctx.clearRect(0, 0, canvas.w, canvas.h);
+		
+		canvas.ctx.globalAlpha = .99;
 		// put pixel data on canvas
-		    //canvas.ctx.putImageData(imageData, 0, 0);
+		//canvas.ctx.putImageData(tempCanvasCTX, 0, 0);
+		canvas.ctx.drawImage(tempCanvas,0,0);
+		canvas.ctx.globalAlpha = 1;
+
+		setTimeout('canvas.fadePreviousData()' , 2000 );
+		
+		
 	}
 
 var c = new Object();
@@ -85,29 +155,42 @@ c.socketScriptComplete = function(){
 c.updateFollower = function(data){
 
 	var coords = data.coordinates.split("," );
-	//trace('id ' + data.id + ' ' + coords[0] +' - ' + coords[1] );
-	
-	//var divid = "f"+data.id.toString();
-	//trace( divid );
-	//trace( $(divid) );
-	
-	$( "#f"+data.id ).css({ left:coords[0], top:coords[1] });
-	
+
+	var _y = parseInt(coords[1]);
+	var _x = parseInt(coords[0]);
+	var _r = data.radius;
+	var _a = data.alpha;
 	
 	canvas.drawPoint({
 						color:data.color, 
-						x:coords[0], 
-						y:coords[1]
+						x:_x, 
+						y:_y,
+						radius:_r,
+						alpha:_a,
 					});
+	
+	
+	_x -= heightOfFollower * .5;
+	_y -= widthOfFollower *  .5;
+	$( "#f"+data.id ).css({ left:_x, top:_y });
+	if( data.name != 'undefined' ){
+		$( "#f"+data.id+" .name").html(data.name);
+	}
+	
 }
 
 c.setupFollower = function(data){
-	var colorVal = data.color;
+	var colorVal = "#"+data.color;
 	var id_val = data.id;
 	
 	trace(' CREATED ' + id_val + ' ----- colorVal: ' + colorVal );
-	$("#followerContainer").append("<div id='f"+id_val+"' class='follow' style='background-color:"+colorVal+"' ></div>" );
+	$("#followerContainer").append("<div id='f"+id_val+"' class='follow'><div class='followIcon' style='background-color:"+colorVal+"' ></div><div class='name'></div></div>" );
 	c.updateFollower(data);
+	
+	var color = toRGB(colorVal);
+	trace(color);
+	
+	
 	trace(data );
 	trace(' /n-----');
 	listOfFollowers.push({id:id_val , color:colorVal});
@@ -128,6 +211,9 @@ c.removeFollower = function(data){
 };
 
 
-
 $(window).resize(function(){ });
 function trace(str){try{console.log(str)}catch(e){};}
+
+
+//https://github.com/daniellmb/HEX-RGB-Conversion
+(function(a){a["toRGB"]=function(a){var b=parseInt(a,16);return[b>>16,b>>8&255,b&255]};a["toHex"]=function(a,b,c){return(c|b<<8|a<<16|1<<24).toString(16).slice(1)}})(this);
--- www/serverside/server.js
+++ www/serverside/server.js
@@ -9,7 +9,12 @@ var fs = require('fs');
 var apps_connected = [];
 var app_index = 0;
 
-var colors = ["#bbd100" , "#45d697" , "#45c7d6" , "#3d81ff" , "#8f3dff" , "#ff3def" , "#f80767" , "#ff3600" , "#c6ff00" , "#00ffde" ];
+var colors = ["bbd100" , "45d697" , "45c7d6" , "3d81ff" , "8f3dff" , "ff3def" , "f80767" , "ff3600" , "c6ff00" , "00ffde" ];
+var defaultRadius = 1;
+var maxRadius = 50;//50;
+var defaultAlpha = 1;//1;
+var minAlpha = 1;//25;
+var maxAlpha = 150;
 
 //-----HTML clients
 io.sockets.on('connection', function (socket) {
@@ -29,7 +34,7 @@ io.sockets.on('connection', function (socket) {
 var serverclient = net.createServer(function (connection) {
 	app_index++;
 	var id = app_index;
-  	
+  					
 	connection.id = id;
 	
 	//var cid = Math.round( Math.random()*colors.length );
@@ -40,8 +45,12 @@ var serverclient = net.createServer(function (connection) {
 	}
 	
 	connection.color = colors[ cid ];
+	connection.coords = [0,0];
+	connection.r = defaultRadius;
+	connection.alpha = defaultAlpha;
 	
-	trace( id + ' color picked ' + connection.color );
+	
+	trace( id + ' color picked ' + connection.color + ' ' + connection.alpha );
 	
 	apps_connected.push(connection);	  
 	connection.write('Echo server\r\n');
@@ -50,7 +59,49 @@ var serverclient = net.createServer(function (connection) {
 	connection.on('data', function(data) {
     	//trace(data);
 	    //sending data via socket io, to HTML clients port 1337
-    	io.sockets.emit('appEvent', { coordinates:data , id: id , color:connection.color });
+	    var _r = defaultRadius;
+	    var _a = defaultAlpha;
+	    
+	    var datasplit = data.split("," );
+	    if( appClient.determineMinimalMovement(datasplit[0],connection.coords[0]) && 
+	    	appClient.determineMinimalMovement(datasplit[1],connection.coords[1]) ){
+	    	
+	    	//enlarge radius.	
+	    	_r = connection.r+((maxRadius-connection.r)*.0008) //r*1.01;
+	    	//lower alpha
+	    	_a = connection.alpha + ((maxAlpha-connection.alpha)*.0005);
+	    	
+	    }else{
+	    	//_r = connection.r*.1;
+	    }
+	    
+	    //check max / min values
+	    if( _r > maxRadius ){
+	    	_r = maxRadius;
+	    }else if( _r < defaultRadius ){
+	    	_r = defaultRadius;
+	    }
+	    	
+	    if( _a < minAlpha ){
+	    	_a = minAlpha;
+	    }else if( _a > maxAlpha ){
+	    	_a = maxAlpha;
+	    }
+	    
+	    var _name = 'undefined';
+	    if( datasplit.length > 3 ){
+	    	trace( datasplit );
+	    	_name = datasplit[3];
+	    }
+	    
+    	io.sockets.emit('appEvent', { coordinates:data , id: id , color:connection.color , radius:_r , alpha:_a , name:_name });
+    	
+    	//store previous content if needed on next update...
+    	connection.coords = datasplit;
+    	connection.r = _r;
+    	connection.alpha = _a;
+    	
+
 	});
 	
 	connection.on('end', function(data) {
@@ -68,6 +119,23 @@ var serverclient = net.createServer(function (connection) {
 
 //server listens to 1338, waiting for data from app
 serverclient.listen(1338, address );
+
+
+var appClient = new Object();
+
+	appClient.determineMinimalMovement = function(current,previous){
+
+		var marginOfError = 5;
+		var _dif = false;
+		
+		//if( ((previous-marginOfError)<current) && (current<(previous+marginOfError))  ){
+		if( previous == current ){
+			_dif = true; 
+		}
+		
+		return _dif;
+	}
+
 
 
 function trace(str){try{console.log(str)}catch(e){};}
